fa89b63721a6cc3e12fa4ba22dc65fd25040c8e7
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2020-11-10 00:27:58 +0200

    Added styles to modal window

--- incident-list-with-actions/src/x-552972-incident-list-with-actions/index.js
+++ incident-list-with-actions/src/x-552972-incident-list-with-actions/index.js
@@ -54,42 +54,44 @@ const view = (state, { updateState }) => {
 			<now-modal size="lg" opened={showModal}
 				content="This is modal text content. It can be set using the 'content' property or as JSX using the default slot."
 				footerActions={[{ label: "Delete", variant: "primary-negative", incidentId: currentIncident.sys_id }]}>
-				<now-input
-					className={'modal__label'}
-					label="Number"
-					type="text"
-					value={currentIncident.number ? currentIncident.number : ''}
-				></now-input>
-				<now-input
-					className={'modal__label'}
-					label="State"
-					type="text"
-					value={currentIncident.state ? currentIncident.short_description : ''}
-				></now-input>
-				<now-input
-					className={'modal__label'}
-					label="Opened At"
-					type="text"
-					value={currentIncident.opened_at ? currentIncident.opened_at : ''}
-				></now-input>
-				<now-input
-					className={'modal__label'}
-					label="Short Description"
-					type="text"
-					value={currentIncident.short_description ? currentIncident.short_description : ''}
-				></now-input>
-				<now-input
-					className={'modal__label'}
-					label="Assignment Group"
-					type="text"
-					value={currentIncident.assignment_group ? currentIncident.assignment_group.display_value : ''}
-				></now-input>
-				<now-input
-					className={'modal__label'}
-					label="Assigned To"
-					type="text"
-					value={currentIncident.assigned_to ? currentIncident.assigned_to.display_value : ''}
-				></now-input>
+				<div className="container">
+					<now-input
+						className={'modal__label'}
+						label="Number"
+						type="text"
+						value={currentIncident.number ? currentIncident.number : ''}
+					></now-input>
+					<now-input
+						className={'modal__label'}
+						label="State"
+						type="text"
+						value={currentIncident.state ? currentIncident.state : ''}
+					></now-input>
+					<now-input
+						className={'modal__label'}
+						label="Opened At"
+						type="text"
+						value={currentIncident.opened_at ? currentIncident.opened_at : ''}
+					></now-input>
+					<now-input
+						className={'modal__label'}
+						label="Short Description"
+						type="text"
+						value={currentIncident.short_description ? currentIncident.short_description : ''}
+					></now-input>
+					<now-input
+						className={'modal__label'}
+						label="Assignment Group"
+						type="text"
+						value={currentIncident.assignment_group ? currentIncident.assignment_group.display_value : ''}
+					></now-input>
+					<now-input
+						className={'modal__label'}
+						label="Assigned To"
+						type="text"
+						value={currentIncident.assigned_to ? currentIncident.assigned_to.display_value : ''}
+					></now-input>
+				</div>
 			</now-modal>
 
 		</div >
